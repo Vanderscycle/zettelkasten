48f1c873a927ca795656dc54533c3e38ca8c4d7f
vanderscycle <henri-vandersleyen@protonmail.com>
2025-12-04 21:21:57 -0800

    update

--- encrypt.nix
+++ encrypt.nix
@@ -10,27 +10,33 @@ pkgs.mkShell {
 
   # nix-shell encrypt.nix
   shellHook = ''
-            ${pkgs.neofetch}/bin/neofetch
-            echo -e "You are now in a dev shell in $(pwd)" | ${pkgs.lolcat}/bin/lolcat
+    ${pkgs.neofetch}/bin/neofetch
+    echo -e "You are now in a dev shell in $(pwd)" | ${pkgs.lolcat}/bin/lolcat
 
-            echo "Available commands:"
-            echo "  encrypt - encrypt files"
-            echo "  decrypt - decrypt files"
+    echo "Available commands:"
+    echo "  encrypt - encrypt files"
+    echo "  decrypt - decrypt files"
 
-            export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
-            encrypt(){
-        # work
-              # sops encrypt ./org/work/work-log.org > ./org/work/enc/work-log.enc.org
-        # home
-              sops encrypt ./org/home/contacts.org > ./org/home/enc/contacts.enc.org
-    exit 0
-            }
+    export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
+    encrypt(){
+      # work
+      sops encrypt ./org/work/work-log.org > ./org/work/enc/work-log.enc.org
+      # home
+      sops encrypt ./org/home/contacts.org > ./org/home/enc/contacts.enc.org
+      sops encrypt ./org/home/meetings.org > ./org/home/enc/meetings.enc.org
+
+      exit 0
+    }
 
 
-            decrypt(){
-              # sops decrypt ./org/work/enc/work-log.enc.org > ./org/work/work-log.org
-              sops decrypt ./org/home/enc/contacts.enc.org > /org/home/contacts.org
-    exit 0
-            }
+    decrypt(){
+      # work
+      sops decrypt ./org/work/enc/work-log.enc.org > ./org/work/work-log.org
+      # home
+      sops decrypt ./org/home/enc/contacts.enc.org > /org/home/contacts.org
+      sops encrypt  ./org/home/enc/meetings.enc.org > ./org/home/meetings.org
+
+      exit 0
+    }
   '';
 }
